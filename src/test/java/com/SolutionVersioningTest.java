79768ca124e8784100139d5a80b940baf6ea9c4d
Bishalj <jaiswalbishal3@gmail.com>
2021-02-23 02:02:31 +0530

    added test

---
+++ src/test/java/com/SolutionVersioningTest.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionVersioningTest {
+
+    @Test
+    void testMain() {
+        // Setup
+
+        // Run the test
+        SolutionVersioning.main(new String[]{"value"});
+
+        // Verify the results
+    }
+}
